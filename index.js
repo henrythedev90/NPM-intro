619aa46b5908de168c4760b92ab86309464e4321
henrythedev90 <henrythedev90@gmail.com>
2024-09-05 12:26:42 -0400

    minor export and import exercises

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+const fs = require("fs");
+const math = require("./math");
+// console.log("this is fs", fs);
+// fs.writeFile("this.txt", "this file is not empty", (err) => {
+//   if (err) {
+//     console.log("this file is not created");
+//   }
+//   console.log("this file has been created");
+// })
+// console.log(math);
+console.log(math.sum(4, 5));
